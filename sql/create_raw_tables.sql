d79f58896ae3ad3da088a262b8780a935291b353
izmfc <izmfc17@gmail.com>
2019-11-30 18:50:24 +0000

    modifying create_raw_tables.sql file

--- sql/create_raw_tables.sql
+++ sql/create_raw_tables.sql
@@ -11,7 +11,6 @@ create table raw.Artists (
   "BeginDate" TEXT,
   "EndDate" TEXT,
   "Wiki_QID" TEXT,
-  "QID" TEXT,
   "ULAN" TEXT
 );
 
@@ -21,40 +20,35 @@ comment on table raw.Artists is 'describes artist characteristics';
 drop table if exists raw.Artworks;
 
 create table raw.Artworks (
-	"Title" TEXT,
-	"Artist" TEXT,
-	"ConstituentID" TEXT,
-	"ArtistBio" TEXT,
-	"Nationality" TEXT,
-	"BeginDate" TEXT,
-	"EndDate" TEXT,
-	"Gender" TEXT,
-	"Date" TEXT,
-	"Medium" TEXT,
-	"Dimensions" TEXT,
-	"CreditLine" TEXT,
-	"AccessionNumber" TEXT,
-	"Classification" TEXT,
-	"Department" TEXT,
-	"DateAcquired" TEXT,
-	"Cataloged" TEXT,
-	"ObjectID" TEXT,
-	"URL" TEXT,
-	"ThumbnailURL" TEXT,
-	"Circumference" TEXT,
-	"Depth" TEXT,
-	"Diameter" TEXT,
-	"Height" TEXT,
-	"Length" TEXT,
-	"Weight" TEXT,
-	"Width" TEXT,
-	"Seat_Height" TEXT,
-	"Duration" TEXT
-
+  "Title" TEXT,
+  "Artist" TEXT,
+  "ConstituentID" TEXT,
+  "ArtistBio" TEXT,
+  "Nationality" TEXT,
+  "BeginDate" TEXT,
+  "EndDate" TEXT,
+  "Gender" TEXT,
+  "Date" TEXT,
+  "Medium" TEXT,
+  "Dimensions" TEXT,
+  "CreditLine" TEXT,
+  "AccessionNumber" TEXT,
+  "Classification" TEXT,
+  "Department" TEXT,
+  "DateAcquired" TEXT,
+  "Cataloged" TEXT,
+  "ObjectID" TEXT,
+  "URL" TEXT,
+  "ThumbnailURL" TEXT,
+  "Circumference" TEXT,
+  "Depth" TEXT,
+  "Diameter" TEXT,
+  "Height" TEXT, 
+  "Length" TEXT,
+  "Weight" TEXT,
+  "Width" TEXT,
+  "Seat_Height" TEXT,
+  "Duration" TEXT
 );
 
-
 comment on table raw.Artworks is 'describes artwork characteristics';
-
-
-
